2358f550c22cadb3f46796dfc82d1ae2da6c8146
Kirk Kornegay <kkornegay1@gmail.com>
2020-09-24 18:15:50 -0400

    24Sep2020 6:15pm

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,12 +1,11 @@
 import os
 import csv
 
-# Set path for file
+
 dirname = os.path.dirname(__file__)
 budgetcsvpath = os.path.join("Resources", "budget_data.csv")
 
 
-# Open the CSV
 with open(budgetcsvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader, None)
@@ -24,3 +23,22 @@ with open(budgetcsvpath) as csvfile:
         total += float(row[1])
 
     print(total)
+
+#def financial_analysis(budget_data):
+
+    #total_months = 0   
+    #for row in csvreader:
+     #   total_months += 1
+
+    #net_total = 0
+    #for row in csvreader:
+   #     net_total += float(row[1])
+
+  #  print(total_months)
+ #   print(net_total)
+
+#with open(budgetcsvpath) as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
+    #next(csvreader, None)
+
+    #financial_analysis(csvreader)
