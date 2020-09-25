18311589ce57773a0b76b89c6e40555a6007102e
Kirk Kornegay <kkornegay1@gmail.com>
2020-09-25 16:30:53 -0400

    25Sep2020 4:30pm

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,15 +1,23 @@
 #import modules
 import os
 import csv
+
 #set path for file
 dirname = os.path.dirname(__file__)
 electioncsvpath = os.path.join("Resources", "Election_data.csv")
+
 #declare variables
 candidates = {}
 total_votes = 0
+max_votes = 0
+winner = ""
+
+#open csv and skip header
 with open(electioncsvpath) as csvfile:
    csvreader = csv.reader(csvfile, delimiter=",")
-   next(csvreader, None)   
+   next(csvreader, None)
+
+#sum total votes and find candidates that received votes
    for row in csvreader:
        total_votes = total_votes + 1
        if row[2] not in candidates:
@@ -20,15 +28,21 @@ print("Election Results")
 print("-----------------------------")        
 print(f"Total Votes: {total_votes}")
 print("-----------------------------")
-max_votes = 0
-winner = ""
+# max_votes = 0
+# winner = ""
 for name,votes in candidates.items():
     print(f"{name}: {100*votes/total_votes:.1f}%: {votes}:")
     if votes> max_votes:
         winner = name
-        max_votes = votes 
-print(f"Winner: {winner}")
+        max_votes = votes
 
+# print("Election Results")  
+# print("-----------------------------")        
+# print(f"Total Votes: {total_votes}")
+print("-----------------------------")
+# print(f"{name}: {100*votes/total_votes:.1f}%: {votes}:")
+print(f"Winner: {winner}")
+print("-----------------------------")
 
 
 
